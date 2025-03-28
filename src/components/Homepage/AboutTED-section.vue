332831881f148634cd2a8202bad64e23bb5d9d5d
elvernh <87465378+elvernh@users.noreply.github.com>
2025-03-29 01:55:43 +0800

    Added about with the pintus

--- src/components/Homepage/AboutTED-section.vue
+++ src/components/Homepage/AboutTED-section.vue
@@ -1,3 +1,74 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import pintuClose from "@/assets/images/PINTU 1.png";
+
+export default defineComponent({
+  setup() {
+    return {
+      pintuClose,
+    };
+  },
+});
+</script>
+
 <template>
-    
+  <div class="about-container">
+    <h1>Learn the Difference!</h1>
+    <div class="doors-container">
+      <div class="door">
+        <h2>TED</h2>
+        <img :src="pintuClose" alt="Closed Door" />
+      </div>
+      <div class="door">
+        <h2>TEDX</h2>
+        <img :src="pintuClose" alt="Closed Door" />
+      </div>
+      <div class="door">
+        <h2>TEDXUCS</h2>
+        <img :src="pintuClose" alt="Closed Door" />
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+.about-container {
+  width: 100vw;
+  height: 150vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+}
+
+h1 {
+  color: white;
+  font-size: 60px;
+  text-align: center;
+}
+
+h2 {
+  color: white;
+  font-size: 60px;
+  text-align: center;
+  font-family: "Panoptica Doesburg";
+}
+
+.doors-container {
+  display: flex;
+  gap: 20px;
+}
+
+.door {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.door img {
+  width: 350px; /* Adjust as needed */
+  height: auto;
+}
+</style>
