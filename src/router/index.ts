7ef37ae449d8ef1567bd982c149af79dc72c754c
Mxiver <122736642+Maiizo@users.noreply.github.com>
2025-04-01 22:56:14 +0700

    tambahin buat paymentMethod dan confirmationPage

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Homepage from '../views/Homepage.vue'
+import BuyTicket from '../views/BuyTicket.vue'
+import PaymentMethod from "../views/PaymentMethod.vue"
+import ConfirmationPage from '../views/ConfirmationPage.vue'
 import Login from "../views/LoginPage.vue"
-import BuyTicket from "../views/BuyTicket.vue"
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,10 +13,13 @@ const router = createRouter({
       name: 'Home',
       component: Homepage,
     },
-    { path: '/buy-ticket', name: 'Buy Ticket', component: BuyTicket },
+    
     // { path: '/gallery', name: 'Gallery', component: Gallery },
     // { path: '/mini-game', name: 'Mini-game', component: MiniGame },
     { path: '/login', name: 'Login', component: Login },
+    { path: '/buy-ticket', name: 'Buy Ticket', component: BuyTicket },
+    { path: '/payment-method', name: 'Payment Method', component: PaymentMethod,  meta: { keepAlive: true }   },
+    { path: '/confirmation-page', name: 'Confirmation Page', component: ConfirmationPage },
   ],
 })
 
